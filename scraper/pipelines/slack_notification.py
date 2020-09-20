9432f17ba744319e089e06484ca0230cef876721
bjoernwe <bjoernwe@users.noreply.github.com>
2020-09-20 23:19:43 +0200

    Move Slack logic to pipeline

--- scraper/pipelines/slack_notification.py
+++ scraper/pipelines/slack_notification.py
@@ -6,7 +6,13 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-from utils.slackpost import post_flat_to_slack
+import os
+
+from itemadapter import ItemAdapter
+from slack import WebClient
+from slack.errors import SlackApiError
+from typing import Optional
+
 from scraper.items import FlatItem
 
 
@@ -22,5 +28,50 @@ class SlackNotificationPipeline:
         pass
 
     def process_item(self, item, spider) -> FlatItem:
-        post_flat_to_slack(item)
+        self._post_flat_to_slack(item)
         return item
+
+    @classmethod
+    def _post_flat_to_slack(cls, flat_item: FlatItem):
+
+        slack = WebClient(token=os.environ['SLACK_API_TOKEN'])
+
+        flat = ItemAdapter(flat_item)
+        rent = f"{int(flat['rent_total'])} (warm) €" if flat['rent_total'] else f"{int(flat['rent_cold'])} €"
+        district_str = f"in {flat['district']}" if flat['district'] else ''
+
+        description = (f"> <{flat['link']}|*{flat['title']}* *[{flat['agency']}]*>\n"
+                       f"> {flat['rooms']} Zimmer ({flat['size']} qm) {district_str}\n"
+                       f"> <https://maps.google.com/?q={flat['address']}|{flat['address']}>\n"
+                       f"> Miete: {rent}"
+                       )
+
+        thumbnail_url = flat['image_urls'][0] if flat['image_urls'] else None
+
+        block = cls._generate_markdown_block(text=description, image_url=thumbnail_url)
+
+        try:
+            slack.chat_postMessage(
+                channel='#new_flats',
+                blocks=[block],
+                icon_url='https://i.imgur.com/OkldsAZ.jpg'
+            )
+        except SlackApiError as e:
+            print(f"Got an error: {e.response['error']}")
+
+    @staticmethod
+    def _generate_markdown_block(text: str, image_url: Optional[str] = None) -> dict:
+        block = {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": text
+            }
+        }
+        if image_url:
+            block['accessory'] = {
+                "type": "image",
+                "image_url": image_url,
+                "alt_text": "Preview"
+            }
+        return block
--- tests/test_slackpost.py
+++ tests/test_slackpost.py
@@ -3,13 +3,14 @@ import pytest
 
 from scraper.items import FlatItem
 from unittest import mock
-from utils.slackpost import post_flat_to_slack
+
+from scraper.pipelines.slack_notification import SlackNotificationPipeline
 
 
 @pytest.fixture
 def flat_item():
-    return FlatItem(id='42', link=None, title='Title', size=100, rooms=3, address='Hauptstr. 1, Berlin', district=None,
-                    rent_cold=500, rent_total=600, image_urls=[])
+    return FlatItem(id='42', agency='Test', link=None, title='Title', size=100, rooms=3, address='Hauptstr. 1, Berlin',
+                    district=None, rent_cold=500, rent_total=600, image_urls=None)
 
 
 @pytest.fixture
@@ -25,5 +26,5 @@ class TestSlackPost:
     def test_flat_item_is_accepted(self, flat_item):
         # GIVEN a FlatItem object
         # WHEN a flat is posted to Slack
-        post_flat_to_slack(flat_item=flat_item)
+        SlackNotificationPipeline._post_flat_to_slack(flat_item=flat_item)
         # THEN no exception was thrown
