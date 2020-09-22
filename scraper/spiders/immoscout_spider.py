7134109dc037d21351662d741aedcd306db01fce
bjoernwe <bjoernwe@users.noreply.github.com>
2020-09-22 22:46:36 +0200

    Add more providers

--- scraper/spiders/immoscout_spider.py
+++ scraper/spiders/immoscout_spider.py
@@ -14,34 +14,39 @@ class ImmoscoutSpider(scrapy.Spider):
     name = "immoscout"
 
     _realtors = {
-        '4_rent_and_sale':      ('1600801303985', 'ade62695f24f3dca58b443c'),
-        'ado':                  ('1600797134665', 'a625521eb5a410e8f57cc'),
-        'carolin_weiss':        ('1600801203632', 'a76ce930e46c42a8975'),
-        'claudia_koehn':        ('1600799502004', 'ac75d7b78eded13d11d'),
-        'cramer_immobilien':    ('1600801252615', 'aa38aca9696dfe2b61a'),
-        'eb':                   ('1600801368708', 'a4f284f473b53cc1765'),
-        'gesobau':              ('1600800319663', 'a11c223a99f96571de5'),
-        'homes_and_service':    ('1600799498871', 'afea67b90e71d7562a2aa8a'),
-        'immo_next':            ('1600799509269', 'ae81f6ec5468cd9'),
-        'stadt_und_land':       ('1600799453223', 'a92a678d73456eed04d'),
-        'stuck_und_fuetting':   ('1600799504774', 'a5532d12e73740bb09b85'),
-        'wbm':                  ('1600799567329', 'a97efe8583d1da8'),
+        '4_rent_and_sale':      'ade62695f24f3dca58b443c',
+        'ado':                  'a625521eb5a410e8f57cc',
+        'carolin_weiss':        'a76ce930e46c42a8975',
+        'claudia_koehn':        'ac75d7b78eded13d11d',
+        'cramer_immobilien':    'aa38aca9696dfe2b61a',
+        'deutsche_wohnen':      'a8f9031eb35e66b77',
+        'eb':                   'a4f284f473b53cc1765',
+        'gcp':                  'a1ff114efd22049c6bd19',
+        'gesobau_1':            'a11c223a99f96571de5',
+        'gesobau_2':            'a6c23d1c589c4ab50ed85',
+        'gesobau_3':            'a13e21300d4dd4ae8fd85ac',
+        'gewobag':              'a54af8451045fd1',
+        'homes_and_service':    'afea67b90e71d7562a2aa8a',
+        'immo_next':            'ae81f6ec5468cd9',
+        'stadt_und_land':       'a92a678d73456eed04d',
+        'stuck_und_fuetting':   'a5532d12e73740bb09b85',
+        'tki':                  'a2e90a5aaf2ba013e406d9e',
+        'wbm':                  'a97efe8583d1da8',
     }
 
     _request_url = 'https://www.immobilienscout24.de/anbieter/api/branchenbuch/v1.0/realestates'
 
     _query_params = {
         'pageNumber': 1,
-        'pageSize': 500,
+        'pageSize': 100,
         'offerType': 'RENT',
         'i': None,
         'encryptedRealtorId': None,
     }
 
     def start_requests(self) -> Iterable[Request]:
-        for name, (immo_id, encrypted_id) in self._realtors.items():
+        for name, encrypted_id in self._realtors.items():
             query_params = dict(self._query_params)
-            query_params['i'] = immo_id
             query_params['encryptedRealtorId'] = encrypted_id
             query_args = urllib.parse.urlencode(query_params)
             url = f'{self._request_url}?{query_args}'
