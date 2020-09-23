3e7178e7760d1665be100e74122cd08d39a47a83
bjoernwe <bjoernwe@users.noreply.github.com>
2020-09-23 17:20:57 +0200

    Update settings

--- settings_filters.py
+++ settings_filters.py
@@ -1,12 +1,15 @@
+from scraper.filters import FlatFilter
+
+
 MUNICIPAL_VENDORS = ['degewo', 'gewobag', 'stadt-und-land', 'wbm', 'immo/degewo', 'immo/gewobau-1', 'immo/gewobau-2',
                      'immo/gewobau-3', 'immo/gewobag', 'immo/stadt&land', 'immo/wbm']
 
 
-EXCLUDED_ZIP_CODES = [13086, 13407]
+EXCLUDED_ZIP_CODES = [10409, 12309, 13086, 13407]
 
 
 SLACK_CHANNELS_FILTERS = {
-    '#all_flats': {},
+    '#all_flats': FlatFilter(),
     '#big': {
         'rooms': (5, None),
         'wbs_required': False,
