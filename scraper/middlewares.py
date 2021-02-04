0ad52bc961db02aa1964a86c994950e4c2763c2b
bjoernwe <bjoernwe@users.noreply.github.com>
2021-02-04 15:21:09 +0100

    Change error formatting

--- scraper/middlewares.py
+++ scraper/middlewares.py
@@ -18,6 +18,6 @@ class SlackExceptionNotificationMiddleware:
         # or Item objects.
         exc_str = repr(exception)
         spider_class = spider.__class__.__name__
-        error_mrkdwn = f'`{exc_str}` from `{spider_class}`:  ```{response}```'
+        error_mrkdwn = f'```{exc_str}```from `{spider_class}`:  ```{response}```'
         error_summary = f'{spider_class}: {exc_str}'
         post_markdown_to_slack(text=error_mrkdwn, channel='#exceptions', preview_text=error_summary)
